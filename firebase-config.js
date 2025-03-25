42f56434ab0ad489af9447c3cfc2feccd5f05a7b
suannhhh <39795602+suannhhh@users.noreply.github.com>
2025-03-25 17:31:47 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC35SC0nF0uKrUXh_JAqCJ6WsfV4jzGHKE",
-  authDomain: "rsvp-8fc6b.firebaseapp.com",
-  projectId: "rsvp-8fc6b",
-  storageBucket: "rsvp-8fc6b.appspot.com",
-  messagingSenderId: "375382418485",
-  appId: "1:375382418485:web:95cfa60c94dfea0844bd7c"
+  apiKey: "AIza",
+  authDomain: "",
+  projectId: "r",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "
 };
 
 // Initialize Firebase
